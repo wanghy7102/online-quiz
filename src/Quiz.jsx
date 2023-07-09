f22bff88b7579fb9e47e371fe10c342a2aa17d9e
Levine Wang <wang_levine@outlook.com>
2023-07-09 11:06:06 -0700

    Shuffle questions and options

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QuizData } from "./QuizData";
 import Question from "./Question";
 
+const shuffle = (arr) => {
+  const result = [...arr];
+  for (let i = 0; i < result.length; i++) {
+    let j = Math.floor(Math.random() * result.length);
+    const temp = result[i];
+    result[i] = result[j];
+    result[j] = temp;
+  }
+  return result;
+};
+
 const Quiz = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    let loadedQuestions = QuizData.questions;
+    loadedQuestions = loadedQuestions.map((question) => {
+      question.options = shuffle(question.options);
+      return question;
+    });
+    loadedQuestions = shuffle(loadedQuestions);
+    setQuestions(loadedQuestions);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +39,7 @@ const Quiz = () => {
         <h2 style={{ marginBottom: "1.5rem" }}>Online Quiz</h2>
       </div>
 
-      {QuizData.questions.map((question, index) => (
+      {questions.map((question, index) => (
         <Question
           key={index}
           label={`${index + 1}. ${question.label}`}
